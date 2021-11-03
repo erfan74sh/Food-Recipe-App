5cf6ec2116eeb85fc4090f3cffc5ca13d5b53209
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-03 14:22:09 +0330

    fetch data in useEffect/set recipes state/fn for search for recipe

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
 import { useState, useEffect } from "react";
 // components
 import Form from "./components/Form";
+// style
+import "./App.scss";
 
 function App() {
+	const apiKey = "9fb125e6a4c34f5db288d1e22f2832ed";
+	let apiCall, data;
+
 	const [recipes, setRecipes] = useState([]);
 	useEffect(() => {
 		const getRecipe = async () => {
-			const apiKey = "9fb125e6a4c34f5db288d1e22f2832ed";
-
-			const apiCall = await fetch(
+			apiCall = await fetch(
 				`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`
 			);
-			const data = await apiCall.json();
-			console.log(data);
+			data = await apiCall.json();
+			// console.log(data);
 			setRecipes(data.results);
 		};
 		getRecipe();
 	}, []);
 
+	const getRecipe1 = async (e) => {
+		e.preventDefault();
+		const query = e.target.elements.recipeName.value;
+		apiCall = await fetch(
+			`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${apiKey}`
+		);
+		data = await apiCall.json();
+		setRecipes(data.results);
+	};
 	return (
 		<div>
-			<Form />
-			{recipes.map((recipe) => {
-				return <p key={recipe.id}>{recipe.title}</p>;
-			})}
+			<Form getRecipe1={getRecipe1} />
+			<div className="query-foods">
+				{recipes.map((recipe) => {
+					return (
+						<div key={recipe.id} className="query-foods__food-card">
+							<img src={recipe.image} alt={recipe.title} />
+							<p>{recipe.title}</p>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
