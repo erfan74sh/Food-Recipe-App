7a47b76924501000f35f80b038913b562a29dd4c
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-14 19:11:25 +0330

    add header of filter component

--- src/components/Header/Filters/Filters.js
+++ src/components/Header/Filters/Filters.js
@@ -1,7 +1,24 @@
 import React from "react";
+// style
+import "./Filters.scss";
 
 const Filters = () => {
-	return <div>content here</div>;
+	return (
+		<div className="filters">
+			<header>
+				<h2>Advance search</h2>
+				<button type="button">
+					<span>show more</span>
+					<span>
+						<i className="fas fa-filter"></i>
+					</span>
+				</button>
+			</header>
+			<form>
+				<input type="text" />
+			</form>
+		</div>
+	);
 };
 
 export default Filters;
