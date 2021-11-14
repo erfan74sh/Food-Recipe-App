5bc853be7589f0994cce030e869f440143a6b17b
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-14 20:06:35 +0330

    add dynamic props to comp

--- src/components/Inputs/InputText/InputText.js
+++ src/components/Inputs/InputText/InputText.js
@@ -1,10 +1,13 @@
 import React from "react";
+// style
+import "../Inputs.scss";
+import "./InputText.scss";
 
-const InputText = () => {
+const InputText = ({ label, placeHolder, width = "17.625rem" }) => {
 	return (
-		<label>
-			label
-			<input type="text" placeholder="place holder" />
+		<label style={{ width: width }}>
+			{label}
+			<input type="text" placeholder={placeHolder} />
 		</label>
 	);
 };
