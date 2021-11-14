8e8bdffc0f34919658b489a99202f78c40adc524
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-14 19:10:06 +0330

    creat InputText component

---
+++ src/components/Inputs/InputText/InputText.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InputText = () => {
+	return (
+		<label>
+			label
+			<input type="text" placeholder="place holder" />
+		</label>
+	);
+};
+
+export default InputText;
