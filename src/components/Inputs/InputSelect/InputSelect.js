ebf83b3126f202a5af783270ca6348b946d1ac2f
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-15 23:50:14 +0330

    add react-select to filter

--- src/components/Inputs/InputSelect/InputSelect.js
+++ src/components/Inputs/InputSelect/InputSelect.js
@@ -1,13 +1,23 @@
 import React from "react";
+import Select from "react-select";
 // style
 import "../Inputs.scss";
 import "./InputSelect.scss";
 
 const InputSelect = ({ label, placeHolder, width = "17.625rem", rMargin }) => {
+	const options = [
+		{ value: "chocolate", label: "Chocolate" },
+		{ value: "strawberry", label: "Strawberry" },
+		{ value: "vanilla", label: "Vanilla" },
+	];
 	return (
 		<label style={{ width: width, marginRight: rMargin }}>
 			{label}
-			<input type="select" placeholder={placeHolder} />
+			<Select
+				options={options}
+				className="select-input-container"
+				classNamePrefix="select-input"
+			/>
 		</label>
 	);
 };
