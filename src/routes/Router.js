bb683b7a3dfb441847266ffa66108c007c0275aa
erfan shafiee <erfan74sh@gmail.com>
2022-04-22 14:33:17 +0430

    add search route to routes

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // components
 import Home from "../pages/Home.js";
+import Search from "../pages/Search.js";
 import Recipe from "../components/Recipe";
 
 const Router = () => {
@@ -9,6 +10,7 @@ const Router = () => {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/search" element={<Search />} />
 				<Route path="/recipe/:recipeId" element={<Recipe />} />
 			</Routes>
 		</BrowserRouter>
